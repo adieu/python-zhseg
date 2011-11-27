e83227a5e674953d9cea3b12de3f314c28f55fcc
adieu <adieu@adieu.cn>
2011-11-27 22:11:21 +0800

    Added some document.

--- zhseg/mmseg.py
+++ zhseg/mmseg.py
@@ -7,6 +7,9 @@ DEFAULT_WORDS_DICT = os.path.join(os.path.dirname(__file__), 'data', 'words.dic'
 
 
 class Algorithm(object):
+    """
+    The algorithm class holds the text to be segmented and returns the token iterier.
+    """
     def __init__(self, text):
         self.text = text 
         self.algorithm = _mmseg.Algorithm(text)
@@ -20,33 +23,33 @@ class Algorithm(object):
 
 
 class Segmenter(object):
-    """docstring for Segmenter"""
+    """
+    The segmenter class manages the dict and segments text.
+    """
     def __init__(self):
         self.dict_loaded = False
 
     def load_characters_dict(self, characters_dict):
-        """docstring for load_character_dict"""
         _mmseg.load_chars(characters_dict)
 
     def load_words_dict(self, words_dict):
-        """docstring for load_word_dict"""
         _mmseg.load_words(words_dict)
 
     def load_dict(self, characters_dict=DEFAULT_CHARACTERS_DICT, words_dict=DEFAULT_WORDS_DICT):
-        """docstring for load_dict"""
         self.load_characters_dict(characters_dict)
         self.load_words_dict(words_dict)
         self.dict_loaded = True
 
     def segment(self, text):
-        """docstring for segment"""
         if not self.dict_loaded:
             self.load_dict()
         return Algorithm(text)
 
 
 def segment(text):
-    """docstring for segment"""
+    """
+    Helper function to segment text with the default segmenter.
+    """
     return segmenter.segment(text)
 
 
